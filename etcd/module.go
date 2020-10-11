ae44502af32b98aabaaa5768d3af7eb84db2d46d
Peter Kozhevnikov <kozhevnikov.peter@gmail.com>
2020-10-12 02:20:19 +0300

    WIP

--- etcd/module.go
+++ etcd/module.go
@@ -2,15 +2,12 @@ package etcd
 
 import (
 	"go.etcd.io/etcd/clientv3"
-
-	custometcd "github.com/uptimus/common/etcd"
 )
 
 type EtcdService struct {
 }
 
-func NewEtcdService(config *custometcd.Config) (*EtcdService, error) {
-
+func NewEtcdService(config *Config) (*EtcdService, error) {
 	_, err := clientv3.New(clientv3.Config{
 		Endpoints:   config.Endpoints,
 		DialTimeout: config.DialTimeout,
