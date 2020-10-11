23aa64255062f4c57bbdcd4abc02cf697ac930be
Peter Kozhevnikov <kozhevnikov.peter@gmail.com>
2020-10-12 02:13:01 +0300

    WIP

--- etcd/module.go
+++ etcd/module.go
@@ -1,1 +1,23 @@
 package etcd
+
+import (
+	"go.etcd.io/etcd/clientv3"
+
+	"github.com/uptimus/common/etcd"
+)
+
+type EtcdService struct {
+}
+
+func NewEtcdService(config *etcd.Config) (*EtcdService, error) {
+
+	_, err := clientv3.New(clientv3.Config{
+		Endpoints:   config.Endpoints,
+		DialTimeout: config.DialTimeout,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &EtcdService{}, nil
+}
